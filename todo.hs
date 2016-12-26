fa483c95a659d103d9a2517a4d4b1f845ee68986
Max Leonard Inden <inden@innoscale.de>
2016-12-26 17:59:13 +0100

    Display help on wrong console parameters

--- todo.hs
+++ todo.hs
@@ -4,28 +4,35 @@ import System.IO
 import Data.List
 import Control.Exception
 
-dispatch :: String -> [String] -> IO ()
-dispatch "add" = add
-dispatch "view" = view
-dispatch "remove" = remove
-
 main :: IO ()
 main = do
-  (command:argList) <- getArgs
-  dispatch command argList
+  arguments <- getArgs
+  parse arguments
+
+parse :: [String] -> IO ()
+parse ("-h" : _) = usage
+parse ("add" : args) = add args
+parse ("view" : args) = view args 
+parse ("remove" : args) = remove args 
+parse [] = usage
+parse _ = usage
 
 add :: [String] -> IO ()
+add [] = usage
 add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+add _ = usage
 
 view :: [String] -> IO ()
+view [] = usage
 view [fileName] = do
   contents <- readFile fileName
   let todoTasks = lines contents
-      numberedTasks = zipWith (\n line -> "[" ++ show n ++ "]  " ++ line)
-                      [0..] todoTasks
+      numberedTasks = zipWith (\n line -> "[" ++ show (n::Int) ++ "]  " ++ line) [0..] todoTasks
   putStr $ unlines numberedTasks
+view _ = usage
 
 remove :: [String] -> IO ()
+remove [] = usage
 remove [fileName, numberString] = do
   contents <- readFile fileName
   let todoTasks = lines contents
@@ -40,3 +47,9 @@ remove [fileName, numberString] = do
       hClose tempHandle
       removeFile "todo.txt"
       renameFile tempName "todo.txt")
+remove _ = usage
+
+usage :: IO ()
+usage = do
+  putStrLn "Usage: todo [-h] [action] [filename] [todo]"
+  putStrLn "Actions: add, view or remove"
